0d6300144e880ec6dce0ff4adc5862fbe67f86ca
saidskander <askander199h@outlook.com>
2020-07-24 19:11:29 +0100

    Done

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - size_t binary_tree_size
+ * @tree: size_t binary_tree_size
+ * Return: const binary_tree_t *tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
