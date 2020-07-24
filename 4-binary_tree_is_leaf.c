f73d5d7c55aab4f6a78bb3bb6a602cc004f67a4c
saidskander <askander199h@outlook.com>
2020-07-24 16:03:49 +0100

    Done

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - int binary_tree_is_leaf
+ * @node : const binary_tree_t *node
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL || node->left != NULL || node->right != NULL)
+return (0);
+return (1);
+}
